f7e63c88b036ad3855f5d97245e39e0718538b08
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-07-16 22:21:48 +0200

    still dealing with eslint

--- app.js
+++ app.js
@@ -4,28 +4,30 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { errors } = require('celebrate');
+const error = require('./middlewares/error');
 const router = require('./routes/index');
 // const auth = require('./middlewares/auth');
 // const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
-const { ERROR_NOT_FOUND } = require('./errors/errors');
-
 const app = express();
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(router);
-app.use('/', (req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: 'we dont have it' });
-});
+// app.use('/', (req, res) => {
+//   res.status(ERROR_NOT_FOUND).send({ message: 'we dont have it' });
+// });
 
 app.use(cookieParser());
 // app.post('/signin', login);
 // app.post('/signup', createUser);
 // app.use(auth);
-
+app.use(errors());
+app.use(error);
 app.listen(PORT, () => {
   console.log(`server on port ${PORT}`);
 });
