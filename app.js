f285d74af8aebb423efebfc9aa0760669e49fa41
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-07-17 14:42:31 +0200

    ghjjk

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const cookieParser = require('cookie-parser');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const helmet = require('helmet');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { errors } = require('celebrate');
 const error = require('./middlewares/error');
 const router = require('./routes/index');
 // const auth = require('./middlewares/auth');
 // const { login, createUser } = require('./controllers/users');
-const helmet = require('helmet');
 
 const { PORT = 3000 } = process.env;
 
@@ -20,7 +19,6 @@ app.use(helmet());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(router);
 
-app.use(cookieParser());
 // app.post('/signin', login);
 // app.post('/signup', createUser);
 // app.use(auth);
